299c50b0e749278c49e7c9c9a5aa2065e9c6f48a
rukmini.b <rukmini.bongu@bitkemy.com>
2024-02-01 18:06:06 +0530

    #0000:Date filed update

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -6,6 +6,8 @@ export function TableData(props: any){
 const handleDeleteRow = (index: number) => {
   deleteRow(index);
 };
+// let date = new Date().toJSON();
+let currentDate = new Date().toJSON().slice(0, 10);
     return(
       <div className='table_main_cont'>
         <div className='table_head_section'>
@@ -53,6 +55,7 @@ const handleDeleteRow = (index: number) => {
           <th>Points</th>
           <th>Status</th>
           <th>Priority</th>
+          <th>Description</th>
           <th>Created On</th>
           <th>Delete</th>
           <th>Edit</th>
@@ -62,12 +65,14 @@ const handleDeleteRow = (index: number) => {
         {newTable.map((item: any, index: any) => {
                   return (
                   <tr key={index}>
-                      <td>{index + 1}</td>
+                      <td>BIT0{`${index + 1}`}</td>
                       <td>{item.title}</td>
                       <td>{item.points}</td> 
                       <td>{item.status}</td>
                       <td>{item.priority}</td>
-                      <td>{item.description}</td>  
+                      <td>{item.description}</td> 
+                      {/* <td>{new Date().getDate()}</td>  */}
+                      <td>{currentDate}</td>
                       <td onClick={() => handleDeleteRow(index)}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                       </svg>
